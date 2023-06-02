f8d153f575f5bf16b8b98bf493156cc3d3b17d7e
kiranvasala24 <87659247+kiranvasala24@users.noreply.github.com>
2023-06-02 11:46:17 +0530

    Create java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+let i;
+let slides = document.getElementsByClassName("mySlides");
+
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";  
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}    
+
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 2000); // Change image every 2 seconds
